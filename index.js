4e6afb4e1abe9f89699b92fb5fe0d768ae9e81dd
Caroline Wangari <karehwachira@gmail.com>
2024-01-12 15:52:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+  mainElement.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const championName = 'Carol';
+newHeader.innerHTML = `Carol is the champion`;
+
+
+
+
+
